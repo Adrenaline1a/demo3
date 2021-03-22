d87e4801d5b95360a774d7957126b7416d1ff794
Adrenaline1 <zligostev.igor123@yandex.ru>
2021-03-22 19:29:54 +0300

    Выполнение всех заданий

---
+++ Задания/numbers.py
@@ -0,0 +1,8 @@
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+num3 = int(input("Введите третье число: "))
+num4 = int(input("Введите четвёртое число: "))
+sum1 = num1+num2
+sum2 = num3+num4
+resul = sum1 / sum2
+print(resul)
